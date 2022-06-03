110efab8577bc4fa0f8e18147baba1c17593a77e
Katja721 <katja.debevc72@gmail.com>
2022-06-03 14:31:12 +0200

    adding hover states

--- script.js
+++ script.js
@@ -13,35 +13,35 @@ const underlineFooter = document.querySelectorAll(".underlineFooter");
 const underlinesFooter = [...underlineFooter];
 const mediaquery = window.matchMedia("(max-width: 500px)").matches;
 
+//implementing header menu hover states
 menuItems.forEach((elem) =>
   elem.addEventListener("mouseover", function () {
     if (mediaquery) return;
     let index = menuItems.indexOf(elem);
-    console.log(underline);
+    elem.style.cursor = "pointer";
     underlines[index].style.display = "block";
     elem.addEventListener("mouseout", function () {
       underlines[index].style.display = "none";
     });
   })
 );
+
+//implementing grid images hover states
 imgs.forEach((img) =>
   img.addEventListener("mouseover", function () {
     if (mediaquery) return;
     img.style.filter = "opacity(0.5)";
+    img.style.cursor = "pointer";
     const index = imgs.indexOf(img);
     imageParagraphs[index].style.color = "hsl(0, 0%, 0%)";
     imageParagraphs[index].style.filter = "opacity(1)";
+    imageParagraphs[index].style.cursor = "pointer";
 
-    // console.log(index);
     img.addEventListener("mouseout", function () {
       if (mediaquery) return;
       imgs[index].style.filter = "opacity(1)";
       imageParagraphs[index].style.color = "hsl(0, 0%, 100%)";
     });
-    // imageParagraphs.forEach(paragraph=>
-    //   paragraph.addEventListener('mouseover', function(){
-
-    //   }))
   })
 );
 
@@ -56,20 +56,18 @@ imageParagraphs.forEach((paragraph) =>
     imgs[index].style.filter = "opacity(0.5)";
     imageParagraphs[index].style.color = "hsl(0, 0%, 0%)";
 
-    // console.log(index);
     paragraph.addEventListener("mouseout", function () {
       if (mediaquery) return;
       imgs[index].style.filter = "opacity(1)";
       imageParagraphs[index].style.color = "hsl(0, 0%, 100%)";
     });
-    // imageParagraphs.forEach(paragraph=>
-    //   paragraph.addEventListener('mouseover', function(){
-
-    //   }))
   })
 );
+
+//immplementing footer hover states
 navItems.forEach((elem) =>
   elem.addEventListener("mouseover", function () {
+    elem.style.cursor = "pointer";
     let index = navItems.indexOf(elem);
     underlinesFooter[index].style.display = "block";
     elem.addEventListener("mouseout", function () {
@@ -77,6 +75,8 @@ navItems.forEach((elem) =>
     });
   })
 );
+
+//immplemnting mobile menu
 const hamburgerIcon = document.querySelector(".hamburger-icon");
 const menu = document.querySelector(".menu");
 const close = document.querySelector(".close-icon");
